494e753de3d0fd490246f43ce659c9a3e328ce28
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-29 22:39:52 +0100

    Socket servo control

---
+++ socket_servo_control.py
@@ -0,0 +1,49 @@
+import socket
+import RPi.GPIO as GPIO
+
+SERVO_PIN = 17
+PORT = 65432
+
+def setupGPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+def start(pwm):
+    new_p = GPIO.PWM(SERVO_PIN, 50)
+    new_p.start(pwm)
+    return new_p
+
+def update_dc(value):
+    p.ChangeDutyCycle(value)
+
+def handleData(data, p):
+    split_data = data.split(":")
+    if split_data[0] == "start":
+        return start(float(split_data[1]))
+    elif split_data[0] == "stop":
+        p.stop()
+    elif split_data[0] == "dc":
+        update_dc(float(split_data[1]))
+
+    return p
+
+print("Setting up GPIO")
+setupGPIO()
+
+print("Starting server on port", PORT)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("", PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
+            p = None
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break;
+                else:
+                    data = data.decode("utf-8")
+                    p = handleData(data, p)
+        print("Lost connection")
