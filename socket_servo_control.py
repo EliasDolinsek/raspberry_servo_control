cbf509d3768ec7758774d480f01e509ec5704d4c
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-11-30 17:01:08 +0100

    Python code cleanup

--- socket_servo_control.py
+++ socket_servo_control.py
@@ -4,19 +4,23 @@ import RPi.GPIO as GPIO
 SERVO_PIN = 17
 PORT = 65432
 
+
 def setupGPIO():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(SERVO_PIN, GPIO.OUT)
+
 
 def start(pwm):
     new_p = GPIO.PWM(SERVO_PIN, 50)
     new_p.start(pwm)
     return new_p
 
+
 def update_dc(value):
     p.ChangeDutyCycle(value)
 
-def handleData(data, p):
+
+def handle_data():
     split_data = data.split(":")
     if split_data[0] == "start":
         return start(float(split_data[1]))
@@ -27,6 +31,7 @@ def handleData(data, p):
 
     return p
 
+
 print("Setting up GPIO")
 setupGPIO()
 
@@ -34,16 +39,18 @@ print("Starting server on port", PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("", PORT))
     s.listen()
+
     while True:
         conn, addr = s.accept()
         with conn:
-            print("Connected by", addr)
             p = None
+            print("Connected by", addr)
+
             while True:
                 data = conn.recv(1024)
-                if not data:
-                    break;
-                else:
+                if data:
                     data = data.decode("utf-8")
-                    p = handleData(data, p)
+                    p = handle_data()
+                else:
+                    break
         print("Lost connection")
